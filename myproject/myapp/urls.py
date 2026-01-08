79446a5a2179683929605f5c6cc39834ec6f578b
vincenttommi <vincenttommikorir@gmail.com>
2026-01-08 06:59:52 +0300

    solved register bug

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,14 +1,10 @@
-from  django.urls import path,include
-from  .views import UserRegistrationView, UserLoginView,UserLogoutView
+from django.urls import path,include
+from .views import UserRegistrationView,UserLoginView,UserLogoutView
 
 
 
 urlpatterns = [
-    path('api/auth/register/', UserRegistrationView.as_view(), name='user-registration'),
-    path('api/auth/login/', UserLoginView.as_view(), name='user-login'),
-    path('api/auth/logout/', UserLogoutView.as_view(), name='user-logout'),
+    path('register/',UserRegistrationView.as_view(), name='user-registration'),
+    path('auth/login/', UserLoginView.as_view(), name='user-login'),
+    path('auth/logout/', UserLogoutView.as_view(), name='user-logout'),
 ]
-
-
-
-
